ba95adcf398c90e58fa268dda79171ac07d201cd
Mihail Mladenov <sekklowz@gmail.com>
2019-04-12 18:16:39 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,20 +1,39 @@
-import tkinter
+from tkinter import *
 
 SETTINGS = {
     "window_title": "RockFall",
     "window_height": "480",
     "window_width": "600",
+    "window_bg_color": "gray",
     "player_height": "20",
     "player_width": "10",
     "player_color": "red"
 }
 
-root = tkinter.Tk()
-window = tkinter.Canvas(root, height = SETTINGS["window_height"], width = SETTINGS["window_width"])
-window.pack()
+class Board(Canvas):
+
+    def __init__(self):
+        super().__init__(width=SETTINGS["window_width"],
+            height=SETTINGS["window_height"],
+            background=SETTINGS["window_bg_color"])
+        
+        self.pack()
+
+class Player(Frame):
+
+    def __init__(self):
+        super().__init__()
+                
+        self.master.title('Player')
+        self.board = Board()
+        self.pack()
+
+def main():
+
+    root = Tk()
+    player = Player()
+    root.mainloop()  
 
 
-
-
-
-root.mainloop()
+if __name__ == '__main__':
+    main()
